2e5bc8f8c25a5c803ecb243aba05a3cd17cf3000
yunbo.ma <yunbo.ma@bytedance.com>
2026-08-08 11:46:25 +0800

    feat: refresh Android guitar app icon
    Co-Authored-By: Aime <aime@bytedance.com>
    Change-Id: I0e6d6490cff49cf5ebd26f5d8cd8a388454f2149

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yunbo.scoreplayer"
         minSdk = 23
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.3.5"
+        versionCode = 18
+        versionName = "1.3.6"
     }
 
     compileOptions {
